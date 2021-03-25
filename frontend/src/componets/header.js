85510ca0c7d0791b0d8f107b4aa7de690be18fdd
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-03-25 19:16:51 -0300

    Create header.js

---
+++ frontend/src/componets/header.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Header = styled.div`
+    width: 890px;
+    height: 300px;
+    border: 1px solid red;
+    text-align:center;
+    margin-top:100px;
+    margin-bottom:50px;
+`;
+
+export default Header;
