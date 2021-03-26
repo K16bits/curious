06eedf7dc90bd284078fb9a75890d4b6d8b47eb2
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-03-25 21:48:22 -0300

    Create card.js

---
+++ frontend/src/componets/card.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+    border: 1px solid red;
+    height: 205px;
+    width: 100%;
+    margin-bottom:10px;
+    background-color:rgb(255, 255, 255);
+`;
+
+export const SubCard = styled.div`
+    border:1px solid red;
+    height: 44px;
+    width:870;
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+
+    img{
+        height:36px;
+        width:36px;
+        border-radius:50%;
+        margin:5px;
+    }
+`;
