dbf301440d7fc11424a98f8c408addd9856caa13
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-03-26 14:13:30 -0300

    Update header.js

--- frontend/src/componets/header.js
+++ frontend/src/componets/header.js
@@ -1,12 +1,29 @@
 import styled from 'styled-components'
 
-const Header = styled.div`
+import svg from '../img/Wavey-Fingerprint.svg'
+
+export const Header = styled.div`
     width: 890px;
-    height: 300px;
-    border: 1px solid red;
+    height: 266px;
+
     text-align:center;
     margin-top:100px;
-    margin-bottom:50px;
+   
+    background-image: url(${svg});
 `;
 
-export default Header;
+export const SubHeader = styled.div`
+    height:50px;
+    width:100%;
+    background-color:#ffff;
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
+    padding:10px;
+    align-items:center;
+`;
+
+export const ColumnInfo = styled.div`
+    display:flex;
+    flex-direction:column;
+`;
