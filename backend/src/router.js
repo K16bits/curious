0aab4542804af0acccdf6f08f1083e4e1dc612d8
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-03-26 18:49:12 -0300

    Update router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const userControl = require('./controllers/userControl')
 
 router.post("/",userControl.store);
+router.post("/storequestion",userControl.storeQuestions);
+router.get("/showquestion",userControl.showQuestion);
 
 module.exports = router;
