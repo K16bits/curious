edd4dc396e4f19e699da4ab77ba1f84f7e2585b5
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-03-26 18:49:18 -0300

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const router = require('./router');
+const cors = require('cors')
 require('dotenv').config();
 app.use(express.json());
 
-
+app.use(cors());
 app.use(router);
 app.listen(process.env.PORT,
     console.log(`Servidor está rodando na porta ${process.env.PORT}`));
