95c45e46f165250c88e44d0236537e6ff6391ec1
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-03-26 18:49:06 -0300

    adicionar e listar perguntas

--- backend/src/controllers/userControl.js
+++ backend/src/controllers/userControl.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Question = require('../models/Question')
 
 module.exports = {
     async store(req,res){
@@ -6,4 +7,15 @@ module.exports = {
         User.create({name,password})
         res.status(200).json({"status":"Deu tudo de bom"})
     },
+    async storeQuestions(req,res){
+        const {questions} = req.body;
+        console.log(questions)
+        Question.create({questions});
+        res.status(200).json({"status":"Deu tudo de bom"})
+    },
+
+    async showQuestion(req,res){
+        const datas = await Question.find();
+        res.json(datas);
+    }
 }
